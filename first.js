e90131ba3b4e959debacd1587a9c748c023cc7e9
larakh51 <larakh51@gmail.com>
2025-10-27 18:04:32 +0200

    Update first.js

--- first.js
+++ first.js
@@ -1,3 +1,6 @@
+//אחמד זוביידאת - 313536633
+// לרה חורי- 211565445
+
 const num = 30;
 let result = 0;
 (num % 2 == 0 || num % 3 == 0 || num % 5 == 0) && (result = 1);
@@ -7,3 +10,4 @@ let result = 0;
   (result = 2);
 num % 2 == 0 && num % 3 == 0 && num % 5 == 0 && (result = 3);
 console.log(result);
+
